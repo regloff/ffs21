0ce5db78edbfb50a321e60864af49767dc3762ce
Stefano Anzolut <stefanoanzolut@gmail.com>
2021-09-28 18:06:07 +0200

    (Minor) Adds data to .gitignore, removed typos

--- retrieve_deserialize.py
+++ retrieve_deserialize.py
@@ -10,11 +10,11 @@ start = time.time()  # start time
 # Retrieve the files from IPFS/ central server
 
 end_serialization = time.time()
-print("Elapsed time to retrive the file is  {}".formatend_serialization - start))
+print("Elapsed time to retrive the file is  {}".format(end_serialization - start))
 
 # Deserialize
 # file = IPFS_DOWNLOAD
-data = pickle.dumps(file)
+# data = pickle.dumps(file)
 
 end_upload = time.time()
 print("Total elapsed time (retrieval + deserialization) is {}".format(end_upload - start))
--- serialize_store.py
+++ serialize_store.py
@@ -5,16 +5,15 @@ start = time.time()  # start time
 FILE = "/data/1mb.txt"
 
 # Serialization
-data = b""
 with open (FILE, "rb") as f:
     data = pickle.loads(f.read())
 
 end_serialization = time.time()
-print("Elapsed time to serialize is  {}".formatend_serialization - start))
+print("Elapsed time to serialize is  {}".format(end_serialization - start))
 
 # Store to IPFS/ central server
 
 end_upload = time.time()
-print("Total elapsed time (serialization + storage) is  {}".format(end - start))
+print("Total elapsed time (serialization + storage) is  {}".format(end_upload - start))
 
 
