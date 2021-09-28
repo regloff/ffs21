58c984d4194b97df057d585dfb7b828342038dca
Stefano Anzolut <stefanoanzolut@gmail.com>
2021-09-28 13:43:44 +0200

    Beginning of scripts

--- retrieve_deserialize.py
+++ retrieve_deserialize.py
@@ -1,6 +1,11 @@
 import time
+import pickle
+import ipfsApi
+
+# >>> api = ipfsApi.Client('127.0.0.1', 5001)
 
 start = time.time()  # start time
+
 
 # Retrieve the files from IPFS/ central server
 
@@ -8,8 +13,10 @@ end_serialization = time.time()
 print("Elapsed time to retrive the file is  {}".formatend_serialization - start))
 
 # Deserialize
+# file = IPFS_DOWNLOAD
+data = pickle.dumps(file)
 
 end_upload = time.time()
-print("Total elapsed time (retrieval + deserialization) is {}".format(end - start))
+print("Total elapsed time (retrieval + deserialization) is {}".format(end_upload - start))
 
 
--- serialize_store.py
+++ serialize_store.py
@@ -1,8 +1,13 @@
 import time
+import pickle
 
 start = time.time()  # start time
+FILE = "/data/1mb.txt"
 
 # Serialization
+data = b""
+with open (FILE, "rb") as f:
+    data = pickle.loads(f.read())
 
 end_serialization = time.time()
 print("Elapsed time to serialize is  {}".formatend_serialization - start))
